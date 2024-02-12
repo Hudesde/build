09b97d644c4baed3887a85912c22854ad1c991bf
Hudesde <93896068+Hudesde@users.noreply.github.com>
2024-02-12 12:18:13 +0000

    Pending changes exported from your codespace

---
+++ controllers/carritocomprascontroller.js
@@ -0,0 +1,56 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.carritocomprascontroller = void 0;
+const database_1 = __importDefault(require("../database")); //acceso a la base de datos
+class CarritoCompraController {
+    mostrarCarritoCompras(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            const respuesta = yield database_1.default.query('SELECT * FROM CarritoCompras WHERE usuarioID = ?', [id]);
+            if (respuesta.length > 0) {
+                res.json(respuesta[0]);
+                return;
+            }
+            res.status(404).json({ 'mensaje': 'Carrito no encontrado' });
+        });
+    }
+    //aqui va el crud
+    crearCarritoCompras(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            //console.log(req.body)
+            const resp = yield database_1.default.query("INSERT INTO CarritoCompras set ?", [req.body]);
+            res.json(resp);
+            //res.json(null);
+        });
+    }
+    actualizarCarritoCompras(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            //console.log(req.params);
+            console.log(id);
+            const resp = yield database_1.default.query("UPDATE CarritoCompras set ? WHERE usuarioID = ?", [req.body, id]);
+            res.json(resp);
+            //res.json(null);
+        });
+    }
+    eliminarCarritoCompras(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            const resp = yield database_1.default.query(`DELETE FROM CarritoCompras WHERE usuarioID = ${id}`);
+            res.json(resp);
+        });
+    }
+}
+exports.carritocomprascontroller = new CarritoCompraController();
