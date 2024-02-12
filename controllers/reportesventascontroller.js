8c267801209bc2c6f8388594eb0fc230452e7df3
Hudesde <93896068+Hudesde@users.noreply.github.com>
2024-02-12 08:34:20 -0600

    Update reportesventascontroller.js

--- controllers/reportesventascontroller.js
+++ controllers/reportesventascontroller.js
@@ -14,55 +14,80 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.reportesventascontroller = void 0;
 const database_1 = __importDefault(require("../database")); //acceso a la base de datos
+
 class ReportesVentasController {
     mostrarReportesVentas(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const { id } = req.params;
-            const respuesta = yield database_1.default.query('SELECT * FROM ReportesVentas WHERE reporteID = ?', [id]);
-            if (respuesta.length > 0) {
-                res.json(respuesta[0]);
-                return;
+            try {
+                const respuesta = yield database_1.default.query('SELECT * FROM ReportesVentas WHERE reporteID = $1', [id]);
+                if (respuesta.rows.length > 0) {
+                    res.json(respuesta.rows[0]);
+                } else {
+                    res.status(404).json({ 'mensaje': 'Reporte de Ventas no encontrado' });
+                }
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ 'mensaje': 'Error interno del servidor' });
             }
-            res.status(404).json({ 'mensaje': 'HistorialCompras no encontrado' });
         });
     }
+
     listarReportesVentasFecha(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const { id } = req.params;
-            const resp = yield database_1.default.query('SELECT * FROM ReportesVentas WHERE fecha = ?', [id]);
-            res.json(resp);
-            if (resp.length > 0) {
-                res.json(resp[0]);
-                return;
+            try {
+                const resp = yield database_1.default.query('SELECT * FROM ReportesVentas WHERE fecha = $1', [id]);
+                res.json(resp.rows);
+                if (resp.rows.length > 0) {
+                    res.json(resp.rows[0]);
+                } else {
+                    res.status(404).json({ 'mensaje': 'Reporte de Ventas no encontrado' });
+                }
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ 'mensaje': 'Error interno del servidor' });
             }
-            res.status(404).json({ 'mensaje': 'HistorialCompras no encontrado' });
         });
     }
-    //aqui va el crud
+
     crearReportesVentas(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            //console.log(req.body)
-            const resp = yield database_1.default.query("INSERT INTO HistorialCompras set ?", [req.body]);
-            res.json(resp);
-            //res.json(null);
+            try {
+                const resp = yield database_1.default.query("INSERT INTO ReportesVentas SET ?", [req.body]);
+                res.json(resp);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ 'mensaje': 'Error interno del servidor' });
+            }
         });
     }
+
     actualizarReportesVentas(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const { id } = req.params;
-            //console.log(req.params);
-            console.log(id);
-            const resp = yield database_1.default.query("UPDATE HistorialCompras set ? WHERE historialID = ?", [req.body, id]);
-            res.json(resp);
-            //res.json(null);
+            try {
+                const resp = yield database_1.default.query("UPDATE ReportesVentas SET ? WHERE reporteID = $1", [req.body, id]);
+                res.json(resp);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ 'mensaje': 'Error interno del servidor' });
+            }
         });
     }
+
     eliminarReportesVentas(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const { id } = req.params;
-            const resp = yield database_1.default.query(`DELETE FROM HistorialCompras WHERE historialID = ${id}`);
-            res.json(resp);
+            try {
+                const resp = yield database_1.default.query('DELETE FROM ReportesVentas WHERE reporteID = $1', [id]);
+                res.json(resp);
+            } catch (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                res.status(500).json({ 'mensaje': 'Error interno del servidor' });
+            }
         });
     }
 }
+
 exports.reportesventascontroller = new ReportesVentasController();
